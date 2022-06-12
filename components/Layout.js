4242aa6eff984b48c0aaf6100838b207df4e2daf
arifpehlivan <arifpehlivanapap@gmail.com>
2022-06-12 23:16:50 +0300

    add products to cart

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,14 @@
-import { AppBar, Container, createTheme, CssBaseline, Link, Switch, ThemeProvider, Toolbar, Typography } from '@material-ui/core';
+import {
+    AppBar,
+    Container,
+    createTheme,
+    CssBaseline,
+    Link, Switch,
+    ThemeProvider,
+    Toolbar,
+    Typography,
+    Badge
+} from '@material-ui/core';
 import Head from 'next/head';
 import React, { useContext } from 'react';
 import useStyles from '../utils/styles';
@@ -7,8 +17,8 @@ import { Store } from '../utils/Store';
 import Cookies from 'js-cookie';
 
 export default function Layout({ title, description, children }) {
-    const {state, dispatch} = useContext(Store);
-    const {darkMode} = state;
+    const { state, dispatch } = useContext(Store);
+    const { darkMode, cart } = state;
     const theme = createTheme({
         typography: {
             h1: {
@@ -22,21 +32,21 @@ export default function Layout({ title, description, children }) {
                 margin: "1rem 0"
             }
         },
-            palette: {
-                type: darkMode ? 'dark' : 'light',
-                primary: {
-                    main: '#f0c000',
-                },
-                secondary: {
-                    main: '#208080',
-                },
+        palette: {
+            type: darkMode ? 'dark' : 'light',
+            primary: {
+                main: '#f0c000',
             },
+            secondary: {
+                main: '#208080',
+            },
+        },
     });
     const classes = useStyles();
     const darkModeChangeHandler = () => {
-        dispatch({type: darkMode ? "DARK_MODE_OFF":"DARK_MODE_ON"});
+        dispatch({ type: darkMode ? "DARK_MODE_OFF" : "DARK_MODE_ON" });
         const newDarkMode = !darkMode;
-        Cookies.set("darkMode", newDarkMode ? "ON":"OFF");
+        Cookies.set("darkMode", newDarkMode ? "ON" : "OFF");
     }
     return (
         <div>
@@ -57,7 +67,10 @@ export default function Layout({ title, description, children }) {
                         <div>
                             <Switch checked={darkMode} onChange={darkModeChangeHandler}></Switch>
                             <NextLink href="/cart" passHref>
-                                <Link>Cart</Link>
+                                <Link>
+                                {cart.cartItems.length > 0 ? (
+                                <Badge color="secondary" badgeContent={cart.cartItems.length}>Cart</Badge>): ("Cart")} 
+                                </Link>
                             </NextLink>
                             <NextLink href="/login" passHref>
                                 <Link>Login</Link>
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,20 +1,30 @@
 import NextLink from "next/link";
 import { Button, Card, Grid, Link, List, ListItem, Typography } from '@material-ui/core';
-import { useRouter } from 'next/router'
-import React from 'react'
+import React, { useContext } from 'react'
 import Layout from '../../components/Layout';
-import data from '../../utils/data';
 import useStyles from "../../utils/styles";
 import Image from "next/image";
 import Product from "../../models/Product";
 import db from "../../utils/db";
+import axios from "axios";
+import { Store } from "../../utils/Store";
 
 export default function ProductScreen(props) {
+    const {state, dispatch} = useContext(Store);
     const {product} = props;
     const classes = useStyles();
     
     if(!product){
         return <div>Product Not Found</div>
+    }
+
+    const addToCartHandler = async () => {
+        const {data} = await axios.get(`/api/products/${product._id}`);
+        if(data.countInStock <= 0){
+            window.alert("Sorry Product is out of stock");
+            return;
+        }
+        dispatch({type: "CART_ADD_ITEM", payload: {...product, quantity: 1}})
     }
     return (
         <Layout title={product.name} description={product.description}>
@@ -68,7 +78,12 @@ export default function ProductScreen(props) {
                                 </Grid>
                             </ListItem>
                             <ListItem>
-                                <Button fullWidth variant="contained" color="primary">Add to cart</Button>
+                                <Button 
+                                    fullWidth 
+                                    variant="contained" 
+                                    color="primary"
+                                    onClick={addToCartHandler}
+                                    >Add to cart</Button>
                             </ListItem>
                         </List>
                     </Card>
