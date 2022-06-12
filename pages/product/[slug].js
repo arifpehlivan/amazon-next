e2c0fbc24d8159ab7953410b9c0ffc3982cdd186
arifpehlivan <arifpehlivanapap@gmail.com>
2022-06-12 18:39:03 +0300

    add fetch prodcuts from api

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -4,15 +4,15 @@ import { useRouter } from 'next/router'
 import React from 'react'
 import Layout from '../../components/Layout';
 import data from '../../utils/data';
-import { mergeClasses } from "@material-ui/core/node_modules/@material-ui/styles";
 import useStyles from "../../utils/styles";
 import Image from "next/image";
+import Product from "../../models/Product";
+import db from "../../utils/db";
 
-export default function ProductScreen() {
+export default function ProductScreen(props) {
+    const {product} = props;
     const classes = useStyles();
-    const router = useRouter();
-    const {slug} = router.query;
-    const product = data.products.find(a => a.slug === slug);
+    
     if(!product){
         return <div>Product Not Found</div>
     }
@@ -76,4 +76,18 @@ export default function ProductScreen() {
             </Grid>
         </Layout>
     )
+}
+
+export async function getServerSideProps(context){
+    const {params} = context;
+    const {slug} = params;
+
+    await db.connect();
+    const product = await Product.findOne({slug}).lean();
+    await db.disconnect();
+    return {
+        props: {
+            product: db.convertDocToObj(product),
+        }
+    }
 }
