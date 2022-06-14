256469d72568660ceaa53a865b14243af5ea40e9
arifpehlivan <arifpehlivanapap@gmail.com>
2022-06-14 23:13:43 +0300

    complete login page

--- components/Layout.js
+++ components/Layout.js
@@ -7,18 +7,23 @@ import {
     ThemeProvider,
     Toolbar,
     Typography,
-    Badge
+    Badge,
+    Button,
+    Menu,
+    MenuItem
 } from '@material-ui/core';
 import Head from 'next/head';
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import useStyles from '../utils/styles';
 import NextLink from "next/link";
+import { useRouter } from "next/router";
 import { Store } from '../utils/Store';
 import Cookies from 'js-cookie';
 
 export default function Layout({ title, description, children }) {
+    const router = useRouter();
     const { state, dispatch } = useContext(Store);
-    const { darkMode, cart } = state;
+    const { darkMode, cart, userInfo } = state;
     const theme = createTheme({
         typography: {
             h1: {
@@ -47,6 +52,23 @@ export default function Layout({ title, description, children }) {
         dispatch({ type: darkMode ? "DARK_MODE_OFF" : "DARK_MODE_ON" });
         const newDarkMode = !darkMode;
         Cookies.set("darkMode", newDarkMode ? "ON" : "OFF");
+    };
+    const [anchorEl, setAnchorEl] = useState(null);
+    const loginClickHandler = (e) => {
+        setAnchorEl(e.currentTarget);
+    };
+    const loginMenuCloseHandler = () => {
+        setAnchorEl(null);
+        // if (redirect) {e, redirect
+        //   router.push(redirect);
+        // }
+    };
+    const logoutClickHandler = () => {
+        setAnchorEl(null);
+        dispatch({type: "USER_LOGOUT"});
+        Cookies.remove("userInfo");
+        Cookies.remove("cartItems");
+        router.push("/");
     }
     return (
         <div>
@@ -68,13 +90,54 @@ export default function Layout({ title, description, children }) {
                             <Switch checked={darkMode} onChange={darkModeChangeHandler}></Switch>
                             <NextLink href="/cart" passHref>
                                 <Link>
-                                {cart.cartItems.length > 0 ? (
-                                <Badge color="secondary" badgeContent={cart.cartItems.length}>Cart</Badge>): ("Cart")} 
+                                    {cart.cartItems.length > 0 ? (
+                                        <Badge color="secondary" badgeContent={cart.cartItems.length}>Cart</Badge>) : ("Cart")}
                                 </Link>
                             </NextLink>
-                            <NextLink href="/login" passHref>
-                                <Link>Login</Link>
-                            </NextLink>
+                            {userInfo ?
+                                (
+                                    <>
+                                        <Button aria-controls="simple-menu"
+                                            aria-haspopup="true"
+                                            onClick={loginClickHandler} className={classes.navbarButton}>{userInfo.name}</Button>
+                                        <Menu
+                                            id="simple-menu"
+                                            anchorEl={anchorEl}
+                                            keepMounted
+                                            open={Boolean(anchorEl)}
+                                            onClose={loginMenuCloseHandler}
+                                        >
+                                            <MenuItem
+                                                onClick={(e) => loginMenuCloseHandler(e, '/profile')}
+                                            >
+                                                Profile
+                                            </MenuItem>
+                                            <MenuItem
+                                                onClick={(e) =>
+                                                    loginMenuCloseHandler(e, '/order-history')
+                                                }
+                                            >
+                                                Order Hisotry
+                                            </MenuItem>
+                                            {userInfo.isAdmin && (
+                                                <MenuItem
+                                                    onClick={(e) =>
+                                                        loginMenuCloseHandler(e, '/admin/dashboard')
+                                                    }
+                                                >
+                                                    Admin Dashboard
+                                                </MenuItem>
+                                            )}
+                                            <MenuItem onClick={logoutClickHandler}>Logout</MenuItem> 
+                                        </Menu>
+
+                                    </>
+                                ) :
+                                (<NextLink href="/login" passHref>
+                                    <Link>Login</Link>
+                                </NextLink>)
+                            }
+
                         </div>
                     </Toolbar>
                 </AppBar>
--- pages/login.js
+++ pages/login.js
@@ -1,11 +1,23 @@
 import { Button, List, ListItem, TextField, Typography, Link } from '@material-ui/core'
-import React, { useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import Layout from '../components/Layout'
 import useStyles from '../utils/styles'
 import NextLink from "next/link"
 import axios from 'axios'
+import { Store } from '../utils/Store'
+import { useRouter } from 'next/router'
+import Cookies from "js-cookie"
 
 export default function Login() {
+    const router = useRouter();
+    const {redirect} = router.query;
+    const { state, dispatch } = useContext(Store);
+    const { userInfo } = state;
+    useEffect(() => {
+        if(userInfo){
+        router.push("/");
+    }
+    },[])
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const classes = useStyles();
@@ -13,7 +25,9 @@ export default function Login() {
         e.preventDefault();
         try {
             const {data} = await axios.post("/api/users/login", {email, password});
-            alert("Success login");
+            dispatch({type:"USER_LOGIN", payload: data});
+            Cookies.set("userInfo",data);
+            router.push(redirect || "/");
         } catch (err) {
             alert(err.response.data ? err.response.data.message : err.message)
         }
