8255b2a0a3e782023925ba0afb8af1e3e6d8e932
arifpehlivan <arifpehlivanapap@gmail.com>
2022-06-14 14:18:40 +0300

    update remove cart items

--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,26 @@ import data from '../utils/data'
 import NextLink from "next/link"
 import db from "../utils/db";
 import Product from "../models/Product";
+import axios from 'axios';
+import { useContext } from 'react';
+import { useRouter } from 'next/router';
+import { Store } from '../utils/Store';
 
 export default function Home(props) {
+    const router = useRouter();
+    const {state, dispatch} = useContext(Store);
     const {products} = props;
+    const addToCarthandler = async (product) => {
+        const existItem = state.cart.cartItems.find(x => x._id === product._id);
+        const quantity = existItem ? existItem.quantity + 1 : 1 ;
+        const {data} = await axios.get(`/api/products/${product._id}`);
+        if(data.countInStock < quantity){
+            window.alert("Sorry Product is out of stock");
+            return;
+        }
+        dispatch({type: "CART_ADD_ITEM", payload: {...product, quantity}});
+        router.push("/cart");
+    }
     return (
         <Layout>
             <div>
@@ -27,7 +44,13 @@ export default function Home(props) {
                                 </NextLink>
                                 <CardActions>
                                     <Typography>${product.price}</Typography>
-                                    <Button size="small" color='primary'>Add To Cart</Button>
+                                    <Button 
+                                        size="small" 
+                                        color='primary'
+                                        onClick={() => addToCarthandler(product)}
+                                    >
+                                        Add To Cart
+                                    </Button>
                                 </CardActions>
                             </Card>
                         </Grid>
